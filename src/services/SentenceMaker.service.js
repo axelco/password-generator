51bfc30b5741486380ab7cfbba51c2a76bf5cb30
Alex <alexandre.rozec@gmail.com>
2023-07-17 08:52:19 +0200

    handle the case of symbols at the end of a word to make it count inside pw generator (spaceSpliter method)

--- src/services/SentenceMaker.service.js
+++ src/services/SentenceMaker.service.js
@@ -4,24 +4,53 @@ class SentenceMaker{
     constructor(sentence){
         this.sentence = sentence
     }
+
+    spaceSpliter(){
+        // We begin to create array by splitting all spaces in the sentence
+        let sentenceArray = this.sentence.split(' ').filter(e => e !== "")
+
+        // Now we check for speical characters inside each item 
+        // If a symbol is at first place or last place of the item
+        // We will split the symbol to be considered as a word in the pw generator
+        // The original word will be also renamed without the symbol
+        sentenceArray.forEach(e => {
+
+            let match = e.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/);
+
+            if(match !== null && e.length > 1){
+                let symbol = match[0]
+                let wordWithoutSymbol = e.replace(symbol, '')
+                let currentIndex = sentenceArray.indexOf(e)
+
+                // We handle the case when symbol is at last position
+                if(match.index + 1 === e.length){
+                    sentenceArray.splice(currentIndex+1, 0, symbol);
+                    sentenceArray.splice(currentIndex, 1, wordWithoutSymbol)
+                }
+                // We handle the case when symbol is at first position
+                if(match.index === 0){
+                    sentenceArray.splice(currentIndex, 1, wordWithoutSymbol)
+                    sentenceArray.splice(currentIndex, 0, symbol);
+                    
+                }
+            }      
+        })    
+        
+        console.log(sentenceArray)
+        return sentenceArray
+    }
     
     generatePassword(){
 
-        const sentenceArray = this.sentence.split(' ').filter(e => e !== "")    
-
-        // TODO
-            // Check if in each array item there a set of letters 
-            // combined with symbols or letter
-            // If so, we split this sentence too
-            // This is avoid user having to add space each time
-                // exemple : "test!"" becomes "t!"" and not just "t"
-
-        // Now for each word found we get 1st letter
-        // Unless it's a number. If isNaN we set all walues input
+        const sentenceArray = this.spaceSpliter()    
         let pw = ''
         sentenceArray.forEach(e => {
-            if( isNaN(parseFloat(e)))
+            
+            if( isNaN(parseFloat(e))){
+                
                 pw += e.charAt(0)
+            }
+
             else 
                 pw += e        
         })        
